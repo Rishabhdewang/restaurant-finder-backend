48fa516a3df0bdd9436212c8a7f113504eb8baaf
Rishabh <48730812+Rishabhdewang@users.noreply.github.com>
2020-10-05 00:43:52 +0530

    deploying

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const bodyParser = require('body-parser');
 const { Model } = require("objection");
 const Knex = require("knex");
 const knexconfig = require('./knexfile');
-const cors = require("cors")
+const cors = require("cors");
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 //initialization
 const knex = Knex(knexconfig[process.env.NODE_ENV || 'development']);
@@ -15,9 +23,14 @@ Model.knex(knex);
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
 // import routes
 app.use("/rest",router)
+
+if(process.env.NODE_ENV = "production") {
+
+}
 
 app.listen(PORT , () => {
     console.log("Listening on port :", +PORT);
--- knexfile.js
+++ knexfile.js
@@ -30,18 +30,15 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'Yelp',
-      user:     'postgres',
-      password: '8085'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL +`?ssl=true`,
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory : './migrations'
+    },
+    useNullAsDefault : true
   }
 
 };
